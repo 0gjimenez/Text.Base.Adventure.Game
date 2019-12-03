25a51cd979e381303600accc393a5b359117a652
1imartinez <42749604+1imartinez@users.noreply.github.com>
2019-12-02 18:44:02 -0600

    Add files via upload

---
+++ map.py
@@ -0,0 +1,615 @@
+###Player starts at 'd4'###
+
+Map_Info(
+  ZONENAME = 'blank'
+  DESCRIPTION = "description"
+  EXAMINATION = 'examine'
+  SOLVE = False
+  UP = 'up', 'north'
+  DOWN = 'down','south'
+  LEFT = 'left','west'
+  RIGHT = 'right','east'
+)
+solved_places = {
+  'a1': False,'a2': False,'a3': False,'a4': False,'a5': False,'a6': False,'a7': False,
+  'b1': False,'b2': False,'b3': False,'b4': False,'b5': False,'b6': False,'b7': False,
+  'c1': False,'c2': False,'c3': False,'c4': False,'c5': False,'c6': False,'c7': False,
+  'd1': False,'d2': False,'d3': False,'d4': True,'d5': False,'d6': False,'d7': False,
+  'e1': False,'e2': False,'e3': False,'e4': False,'e5': False,'e6': False,'e7': False,
+  'f1': False,'f2': False,'f3': False,'f4': False,'f5': False,'f6': False,'f7': False,
+  'g1': False,'g2': False,'g3': False,'g4': False,'g5': False,'g6': False,'g7': False,  
+}
+
+zonemap{
+  'a1': {
+    'a1'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }'a2': {
+    'a2'{
+      ZONENAME = 'blank'
+    DESCRIPTION = 'description'
+    EXAMINATION = 'examine'
+    SOLVE = False
+    UP = 'up', 'north'
+    DOWN = 'down','south'
+    LEFT = 'left','west'
+    RIGHT = 'right','east'
+    }
+  }
+  'a3': {
+    'a3'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'a4': {
+    'a4'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'a5': {
+    'a5'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'a6': {
+    'a6'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'a7': {
+    'a7'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+    RIGHT = 'right','east'
+    }
+  }
+  'b1': {
+    'b1'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'b2': {
+    'b2'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'b3': {
+    'b3'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'b4': {
+    'b4'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'b5': {
+    'b5'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'b6': {
+    'b6'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'b7': {
+    'b7'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+   'c1': {
+    'c1'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'c2': {
+    'c2'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'c3': {
+    'c3'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'c4': {
+    'c4'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'c5': {
+    'c5'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'c6': {
+    'c6'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'c7': {
+    'c7'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  
+  'd1': {
+    'd1'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'd2': {
+    'd2'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'd3': {
+    'd3'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'd4': {
+    'd4'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'd5': {
+    'd5'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'd6': {
+    'd6'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'd7': {
+    'd7'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'e1':{
+    'e1'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  } 
+  'e2':{
+    'e2'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }  
+   'e3':{
+    'e3'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }  
+   'e4':{
+    'e4'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }  
+   'e5':{
+    'e5'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }  
+   'e6':{
+    'e6'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }  
+   'e7':{
+    'e7'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'f1':{
+    'f1'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  } 
+  'f2':{
+    'f2'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  } 
+  'f3':{
+    'f3'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  } 
+  'f4':{
+    'f4'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  } 
+  'f5':{
+    'f5'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  } 
+  'f6':{
+    'f6'{
+      ZONENAME = 'blank'
+     DESCRIPTION = 'description'
+     EXAMINATION = 'examine'
+     SOLVE = False
+     UP = 'up', 'north'
+     DOWN = 'down','south'
+     LEFT = 'left','west'
+     RIGHT = 'right','east'
+    }
+  } 
+  'f7':{
+    'f7'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'g1':{
+    'g1'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'g2':{
+    'g2'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'g3':{
+    'g3'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'g4':{
+    'g4'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'g5':{
+    'g5'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'g6':{
+    'g6'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+  'g7':{
+    'g7'{
+      ZONENAME = 'blank'
+      DESCRIPTION = 'description'
+      EXAMINATION = 'examine'
+      SOLVE = False
+      UP = 'up', 'north'
+      DOWN = 'down','south'
+      LEFT = 'left','west'
+      RIGHT = 'right','east'
+    }
+  }
+
+}
+
+print(solved_places)
