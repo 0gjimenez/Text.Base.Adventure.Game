89c04b7a716957e9eb69f990b587ef295b7ba253
1imartinez <42749604+1imartinez@users.noreply.github.com>
2019-12-09 21:07:09 -0600

    Map Incompletion
    I am getting close in finishing up the map.py. It will take longer hopefully no more than an hour and 30 minutes more

--- map.py
+++ map.py
@@ -1,615 +1,107 @@
-###Player starts at 'd4'###
+level = {}
 
-Map_Info(
-  ZONENAME = 'blank'
-  DESCRIPTION = "description"
-  EXAMINATION = 'examine'
-  SOLVE = False
-  UP = 'up', 'north'
-  DOWN = 'down','south'
-  LEFT = 'left','west'
-  RIGHT = 'right','east'
-)
-solved_places = {
-  'a1': False,'a2': False,'a3': False,'a4': False,'a5': False,'a6': False,'a7': False,
-  'b1': False,'b2': False,'b3': False,'b4': False,'b5': False,'b6': False,'b7': False,
-  'c1': False,'c2': False,'c3': False,'c4': False,'c5': False,'c6': False,'c7': False,
-  'd1': False,'d2': False,'d3': False,'d4': True,'d5': False,'d6': False,'d7': False,
-  'e1': False,'e2': False,'e3': False,'e4': False,'e5': False,'e6': False,'e7': False,
-  'f1': False,'f2': False,'f3': False,'f4': False,'f5': False,'f6': False,'f7': False,
-  'g1': False,'g2': False,'g3': False,'g4': False,'g5': False,'g6': False,'g7': False,  
-}
+starting_position = (0, 0)
 
-zonemap{
-  'a1': {
-    'a1'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }'a2': {
-    'a2'{
-      ZONENAME = 'blank'
-    DESCRIPTION = 'description'
-    EXAMINATION = 'examine'
-    SOLVE = False
-    UP = 'up', 'north'
-    DOWN = 'down','south'
-    LEFT = 'left','west'
-    RIGHT = 'right','east'
-    }
-  }
-  'a3': {
-    'a3'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'a4': {
-    'a4'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'a5': {
-    'a5'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'a6': {
-    'a6'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'a7': {
-    'a7'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-    RIGHT = 'right','east'
-    }
-  }
-  'b1': {
-    'b1'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'b2': {
-    'b2'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'b3': {
-    'b3'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'b4': {
-    'b4'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'b5': {
-    'b5'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'b6': {
-    'b6'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'b7': {
-    'b7'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-   'c1': {
-    'c1'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'c2': {
-    'c2'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'c3': {
-    'c3'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'c4': {
-    'c4'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'c5': {
-    'c5'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'c6': {
-    'c6'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'c7': {
-    'c7'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
+class Map:
+  def __init__(self, name, x, y, intro, description)
+    self.name = name
+    self.x = x
+    self.y = y
+    self.intro = intro
+    self.description = description
   
-  'd1': {
-    'd1'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'd2': {
-    'd2'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'd3': {
-    'd3'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'd4': {
-    'd4'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'd5': {
-    'd5'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'd6': {
-    'd6'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'd7': {
-    'd7'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'e1':{
-    'e1'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  } 
-  'e2':{
-    'e2'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }  
-   'e3':{
-    'e3'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }  
-   'e4':{
-    'e4'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }  
-   'e5':{
-    'e5'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }  
-   'e6':{
-    'e6'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }  
-   'e7':{
-    'e7'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'f1':{
-    'f1'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  } 
-  'f2':{
-    'f2'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  } 
-  'f3':{
-    'f3'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  } 
-  'f4':{
-    'f4'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  } 
-  'f5':{
-    'f5'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  } 
-  'f6':{
-    'f6'{
-      ZONENAME = 'blank'
-     DESCRIPTION = 'description'
-     EXAMINATION = 'examine'
-     SOLVE = False
-     UP = 'up', 'north'
-     DOWN = 'down','south'
-     LEFT = 'left','west'
-     RIGHT = 'right','east'
-    }
-  } 
-  'f7':{
-    'f7'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'g1':{
-    'g1'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'g2':{
-    'g2'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'g3':{
-    'g3'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'g4':{
-    'g4'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'g5':{
-    'g5'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'g6':{
-    'g6'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
-  'g7':{
-    'g7'{
-      ZONENAME = 'blank'
-      DESCRIPTION = 'description'
-      EXAMINATION = 'examine'
-      SOLVE = False
-      UP = 'up', 'north'
-      DOWN = 'down','south'
-      LEFT = 'left','west'
-      RIGHT = 'right','east'
-    }
-  }
+  def intro_text(self):
+    return self.intro
 
-}
+  def adjacent_moves(self):
+    north = tile_exists(self.x, self.y - 1).name
+    south = tile_exists(self.x , self.y + 1).name
+    east = tile_exists(self.x + 1, self.y).name
+    west = tile_exists(self.x - 1, self.y).name
+    moves = []
 
-print(solved_places)
+    if north != None:
+      return moves
+  def available_actions(self, **kwargs):
+    if kwargs:
+      moves = []
+      for key, value in kwargs.items():
+        if key == "in_battle":
+            if value == True:
+              pass
+        if key == "in_town":
+            if value == True:
+              print("In town, from available actions")
+              pass
+      else:
+        moves = self.adjacent_moves()
+        return moves
+
+class SpawnPoint(Map):
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.name = "Spawn"
+    self.intro = "A spawin"
+    self.description = "Spawn place"
+    super().__init__(
+      name = self.name,
+      x = self.x,
+      y = self.y,
+      intro = self.intro,
+      description = self.description
+    )
+  
+  def modify_player(self, player):
+    pass
+
+  
+
+def load_tiles():
+  with open("World.txt", "r") as f:
+      rows = f.readlines()
+    x_max = len(rows[0].split("\t"))
+    for y in range(len(rows)):
+        cols = rows[y].split("\t")
+
+        try:
+          for x in range(x_max):
+            title_name = cols[x].replace("\n", "")
+
+              if title_name == "":
+                  level[(x, y)] = None
+              else:
+                  if title_name == "SpawnTile":
+                        level[(x, y)] = SpawnTile(x, y)
+                        global starting_position
+                        starting_position = (x, y)
+                    if title_name == "ForestTile":
+                        level[(x, y)] = ForestTile(x, y)
+                    if title_name == "MountainTile":
+                        level[(x, y)] = MountainTile(x, y)
+                    if title_name == "PlainsTile":
+                        level[(x, y)] = PlainsTile(x, y)
+                    if title_name == "FairyTile":
+                        level[(x, y)] = FairyTile(x, y)
+                    if title_name == "SwampTile":
+                        level[(x, y)] = SwampTile(x, y)
+                    if title_name == "ForestDungeon":
+                        level[(x, y)] = ForestDungeon(x, y)
+                    if title_name == "MountainDungeon":
+                        level[(x, y)] = MountainDungeon(x, y)
+                    if title_name == "PlainsDungeon":
+                        level[(x, y)] = PlainsDungeon(x, y)
+                    if title_name == "FairyDungeon":
+                        level[(x, y)] = FairyDungeon(x, y)
+                    if title_name == "SwampDungeon":
+                        level[(x, y)] = SwampDungeon(x, y)
+        except Exception as e:
+            if e == IndexError:
+                print("Index done not exist!!")
+            else:
+                print(e)
+                quit()
+
+
+def title_exist(x, y):
+    return level.get((x, y))
